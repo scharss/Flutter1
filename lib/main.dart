115a9eb2abbf422df9aa9cf1903a8a32e2fea51d
Scharss <scharss@gmail.com>
2019-11-17 01:00:08 -0500

    primero
    primero

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+
+void main()  => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('EasyList'),
+        ),
+        body: Card(child: Column(children: <Widget>[
+          Image.asset('assets/4.jpg'),
+          Text('Baphomet')
+        ],),),
+      ),
+    );
+  }
+}
